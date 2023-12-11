a2dc16762b9cf914b01a0b958ff1b8d9bf96d64a
dobrimate97 <dobrics89@gmail.com>
2023-12-11 12:31:28 +0100

    Reading all values from the file

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+all_values = [] 
 with open('account.txt', 'r') as accounts:
     for line in accounts:
         values = line.strip().split(';')
-for value in values:
+        all_values.extend(values)
+for value in all_values:
     print(value)
