60fe3c96ba67b9e6aa5c7300bd1ca10a26a4accc
dobrimate97 <dobrics89@gmail.com>
2023-12-11 13:49:30 +0100

    Defining register button by id

--- formFilling.py
+++ formFilling.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
 browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-browser.get("file:///C:/bot/registerPage.html")
+browser.get("https://adidas.de")
+register_button = browser.find_element(By.ID, 'register-modal')
