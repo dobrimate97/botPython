0e773107d666c0f7f15b88e50ae9ccd6adb65777
dobrimate97 <dobrics89@gmail.com>
2023-12-13 14:24:45 +0100

    Trying to avoid detection

--- formFilling.py
+++ formFilling.py
@@ -8,12 +8,25 @@ import time
 # TODO: FUNCKIÓKBA RENDEZNI MINDENT 
 options = Options()
 options.add_experimental_option("excludeSwitches",["enable-automation"])
+options.add_argument('--disable-blink-features=AutomationControlled')
+options.add_experimental_option('useAutomationExtension', False)
+browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)   
+browser.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+browser.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
+    
 
-browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 browser.get("https://adidas.de")
-time.sleep(5)
+time.sleep(5) #WAIT FOR SELECTOR?
 
 browser.find_element(By.XPATH,"//span[text()='I accept functional and marketing cookies etc.']").click()
+#browser.execute_script("window.scrollTo(0,document.documentElement.scrollHeight)")
+#time.sleep(2)
+#browser.execute_script("window.scrollTo(0,-document.documentElement.scrollHeight)")
+#browser.execute_script("window.scrollTo(0,document.documentElement.scrollHeight)")
+#time.sleep(2)
+#browser.execute_script("window.scrollTo(0,-document.documentElement.scrollHeight)")
+#time.sleep(5)
+
 browser.find_element(By.LINK_TEXT, 'werde mitglied').click()
 time.sleep(3)
 
@@ -21,4 +34,4 @@ email = browser.find_element(By.ID, "email")
 email.send_keys(account_data[2])
 browser.find_element(By.XPATH,"//span[text()='WEITER']").click()
 
-time.sleep(20)
+time.sleep(200)
